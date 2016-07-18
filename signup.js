5823a600fe447bd842416b84416cd53f17b42feb
Arik <erikbrodch@gmail.com>
2016-07-18 08:44:25 +0300

    finished almost all tasks on DOM lesson w2d1

--- signup.js
+++ signup.js
@@ -18,3 +18,36 @@ var validationPassword = function () {
 };
 
 document.getElementById("passwordTyped").onblur = validationPassword;
+
+//add code to validate passwords are the same
+
+var passwordSimilarity = function() {
+
+var password1 = document.getElementById("passwordTyped").value;
+var password2 = document.getElementById("confirm_password").value;
+
+if (password1 != password2) {
+		document.getElementById('msg').innerHTML = "passwords don't match";
+		document.getElementById('msg').style.backgroundColor = "red";
+	} 
+
+}
+document.getElementById("confirm_password").onblur = passwordSimilarity;
+
+//validate if country is Israel o US
+
+var countryValidation = function () {
+	alert("2");
+	var myCountry = document.getElementById("country").value;
+	if (myCountry != "USA" || myCountry != "Israel") {
+		document.getElementById('msg').innerHTML = "not the right country";
+		document.getElementById('msg').style.backgroundColor = "red";
+	}
+
+};
+
+document.getElementById("country").onblur = countryValidation;
+
+
+
+
